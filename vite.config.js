0edee20cb46d1e75017684b9a235facd30ac9d94
Viper-Tol <tendonkakami@outlook.com>
2026-03-17 18:05:14 +0800

    更新

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/-123/',
+  base: '/-123/',
   plugins: [
     vue(),
     vueDevTools(),
